2ef08c358958cfcf140893046a8fd7a1913addec
john doe <scottbruceburton@gmail.com>
2020-09-12 18:47:06 -0400

    todo structure

---
+++ mapit.py
@@ -0,0 +1,6 @@
+#! python3
+# TODO: gets a street adress from teh command line arguments or clipboard
+# TODO: opens web browser to the Google Maps page for the address.
+# TODO: Read the command line arguments from sys.argv
+# TODO: Read the clipboard contents
+# TODO: Call the webbrowser.open() function to open the web browser
